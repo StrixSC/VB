5f374012cfea7d8aa41a051175c7bef261025320
StrixSC <talon.strix@gmail.com>
2021-02-21 23:58:47 -0500

    rust start + update to generator

--- gen-blocklet.sh
+++ gen-blocklet.sh
@@ -1,10 +1,10 @@
 BLOCKLET_FOLDER="blocklets/$1"
 mkdir -p $BLOCKLET_FOLDER
-echo -e "[$BLOCKLET_FOLDER/$2]\n$(cat ./example-blocklet-conf)" > $BLOCKLET_FOLDER/example-config
+echo -e "[$1/$2]\n$(cat ./example-blocklet-conf)" > $BLOCKLET_FOLDER/example-config
 echo "Blocklet folder created at $BLOCKLET_FOLDER" 
 echo "Creating blocklet entry"
 cp ./default-blocklet $BLOCKLET_FOLDER/$2
 chmod +x "$BLOCKLET_FOLDER/$2"
 
 echo "Blocklet creation complete"
-cd $BLOCKLET_FOLDER
+cd "$BLOCKLET_FOLDER"
